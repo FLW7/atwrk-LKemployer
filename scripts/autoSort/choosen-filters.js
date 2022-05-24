cc53df207f775ecc43cc8d74c8cbefcd4ef7fc70
Timur <93697574+FLW7@users.noreply.github.com>
2022-05-24 16:42:16 +0300

    Update choosen-filters.js

--- scripts/autoSort/choosen-filters.js
+++ scripts/autoSort/choosen-filters.js
@@ -124,7 +124,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // -------------------------------------------------
         // добавление иконки "очистить"
         let pic = document.createElement("IMG");
-        pic.src = "../responses/img/filter-del.svg";
+        pic.src = "./img/filter-del.svg";
         pic.classList.add('img-close-current')
         filterItem.appendChild(pic);
         // добавление элемента в список выбранных фильтров
